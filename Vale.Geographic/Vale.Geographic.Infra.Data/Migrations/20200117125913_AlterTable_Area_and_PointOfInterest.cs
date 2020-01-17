f4e5f3d999b5fbc393945614d167bfcff3ab10e1
Jessica Gramelisch <c0608817@vale.com>
2020-01-17 14:13:09 -0300

    feat: 🎸 Create migration script

---
+++ Vale.Geographic/Vale.Geographic.Infra.Data/Migrations/20200117125913_AlterTable_Area_and_PointOfInterest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vale.Geographic.Infra.Data.Migrations
+{
+    public partial class AlterTable_Area_and_PointOfInterest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AreaId",
+                table: "PointOfInterest",
+                nullable: true,
+                oldClrType: typeof(Guid));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Icon",
+                table: "PointOfInterest",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Color",
+                table: "Area",
+                type: "varchar(255)",
+                maxLength: 255,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Icon",
+                table: "PointOfInterest");
+
+            migrationBuilder.DropColumn(
+                name: "Color",
+                table: "Area");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "AreaId",
+                table: "PointOfInterest",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldNullable: true);
+        }
+    }
+}
