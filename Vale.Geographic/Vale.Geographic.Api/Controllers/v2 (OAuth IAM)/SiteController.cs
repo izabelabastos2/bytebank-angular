cef3cf2941762df13d666370334382763331e9e4
João Pedro Lonczynski <lonczynski.joao@gmail.com>
2020-09-22 14:26:41 -0300

    testing env dev

--- Vale.Geographic/Vale.Geographic.Api/Controllers/v2 (OAuth IAM)/SiteController.cs
+++ Vale.Geographic/Vale.Geographic.Api/Controllers/v2 (OAuth IAM)/SiteController.cs
@@ -17,6 +17,7 @@ namespace Vale.Geographic.Api.Controllers.v2
     /// Controller to Sites
     /// </summary>
     [Route("api/Site")]
+    [ApiVersion("2")]
     public class SiteController : Controller
     {
         readonly ISiteAppService siteAppService;
